9e3ee9d9bebd687e463906e6ebdfdf8fd11f1caa
JonOrtiz451 <90286340+JonOrtiz451@users.noreply.github.com>
2021-10-19 16:30:03 -0600

    commit1

---
+++ Consuming Services/main.js
@@ -0,0 +1,12 @@
+// JavaScript source code
+function analyzetext() {
+    var text = document.getElementById('texttoanalyze').value;
+    console.log(text);
+
+}
+
+function analyzeImg() {
+    var urlImage = document.getElementById('urlImgtoAnalyze').value;
+    console.log(urlImage);
+
+}
